122acac049756455d12f8dab43d40c0fd5cb383b
Anton Allo <qqqqton1@yandex.ru>
2024-03-16 14:53:43 +0300

    add loaded countries and toggle theme dark/light

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './index.css';
 import App from './App';
 
-
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
---
+++ src/store/index.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { configureStore } from '@reduxjs/toolkit';
+import { themeReducer } from './theme/theme-reducer';
+import { countriesReducer } from './countries/countries-reducers';
+
+// import { rootReducer } from './root-reducer';
+import * as api from '../config';
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const store = createStore(
+//   rootReducer,
+//   composeEnhancers(
+//     applyMiddleware(
+//       thunk.withExtraArgument({
+//         client: axios,
+//         api,
+//       })
+//     )
+//   )
+// );
+const store = configureStore({
+  reducer: {
+    theme: themeReducer,
+    countries: countriesReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: {
+          client: axios,
+          api,
+        },
+      },
+    }),
+  // devTools: process.env.NODE_ENV !== 'production',
+  devTools:
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__(),
+});
+export default store;
